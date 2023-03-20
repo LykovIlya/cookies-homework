d59d7abe488b4e7a7aee754581bd09d7ff8416a1
Lykov Ilya <ilya.lykov.program@gmail.com>
2023-03-20 16:33:14 +0400

    add: work with shopping cart

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -11,6 +11,73 @@ document.addEventListener("DOMContentLoaded", function () {
     let wrapperProducts = document.querySelector(".wrapper_cards");
     let shoppingCartWrapper = document.querySelector(".shoppingCartCards_wrapper");
 
+    // =================================================================
+
+    //ProductsCart
+    let productsCartArray = [];
+
+    function addProduct(product) {
+        if (!isProductInCart(product)) {
+            productsCartArray.push(product);
+        } else {
+            product.count += 1;
+        }
+    }
+    function isProductInCart(product) {
+        productsCartArray.forEach(productIter => {
+            if (productIter.equals(product)) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+
+
+
+    class ItemCartProduct {
+        constructor(id) {
+            this.id = id;
+            this.count = 1;
+            this.classes = ["products__item"];
+            this.containerElement = shoppingCartWrapper;
+        }
+        getId() {
+            return this.id;
+        }
+        equals(product) {
+            if (this.getId() == product.getId()) {
+                return true;
+            }
+            return false;
+        }
+        render() {
+            const div = document.createElement("div");
+            this.classes.forEach(className => div.classList.add(className));
+            div.innerHTML = `<div data-id="${this.id}" class="shoppingCartCard card">
+						<div class="card card_descr">Изделие с ID ${this.id}</div>
+						<input value="1" class="card card_count"></input>
+						<div class="card card_button add">
+							<button>+</button>
+						</div>
+						<div class="card card_button remove">
+							<button>-</button>
+						</div>
+					</div>`;
+            shoppingCartWrapper.appendChild(div);
+        }
+
+    }
+    function getInputCountById(id) {
+        let input = shoppingCartWrapper.querySelector(`[data-id="${id}"]`);
+        return input.innerText;
+    }
+
+
+
+    // =================================================================
+
+    //ProductsList
     class ItemProducts {
         constructor(idProduct) {
             this.id = idProduct;
@@ -30,29 +97,6 @@ document.addEventListener("DOMContentLoaded", function () {
             // console.log(div.innerText);
         }
     }
-    class ItemCartProducts {
-        constructor(id) {
-            this.id = id;
-            this.count = 1;
-            this.classes = ["products__item"];
-            this.containerElement = shoppingCartWrapper;
-        }
-        render() {
-            const div = document.createElement("div");
-            this.classes.forEach(className => div.classList.add(className));
-            div.innerHTML = `<div class="shoppingCartCard card">
-						<div class="card card_descr">Изделие с ID ${this.id}</div>
-						<input value="1" class="card card_count"></input>
-						<div class="card card_button add">
-							<button>+</button>
-						</div>
-						<div class="card card_button remove">
-							<button>-</button>
-						</div>
-					</div>`;
-        }
-    }
-
     const getResourse = async function (url) {
 
         let res = await fetch(url);
@@ -69,15 +113,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 responseDiv.innerHTML = "";
                 responseDiv.innerHTML = result;
             });
-        // if (response.ok) {
-        //     responseDiv.classList.add("green");
-        // } else {
-        //     responseDiv.classList.add("red");
-        // }
     });
 
-    let productsArray = [];
-    let addToCartButtons = [];
+    let productsArray = []; // Array of products
+
     getResourse("ajax-products-servlet").
         then(data => {
             // console.log(data.products);
@@ -86,15 +125,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 new ItemProducts(id).render();
             });
         }).then(() => {
+            let addToCartButtons = []; //buttons for eventListeners to add to cart
+            //add the products to the productsCart array
             addToCartButtons = document.querySelectorAll('.add-to-cart-button');
-            console.log(addToCartButtons);
 
             addToCartButtons.forEach(button => {
                 button.addEventListener('click', (event) => {
-                    console.log(button.dataset.id);
+                    let itemCartProduct = new ItemCartProduct(productsArray[button.dataset.id - 1].id);
+                    console.log(itemCartProduct);
+                    addProduct(itemCartProduct);
+                    itemCartProduct.render();
+
                 });
 
             });
         });
+
+
 
 });
